210569249865c4d7b834510397e0d7b8903254b4
JiHoonKwak9907 <101690974+JiHoonKwak9907@users.noreply.github.com>
2022-05-02 00:24:44 +0900

    Delete 개인포트폴리오 directory

--- 개인포트폴리오/app.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.querySelector('.checkbox');
-console.log(checkbox);
-
-checkbox.addEventListener('click',toggleClick);
-
-function toggleClick(){
-  document.body.classList.toggle('dark');
-  console.log('Working!');
-}
